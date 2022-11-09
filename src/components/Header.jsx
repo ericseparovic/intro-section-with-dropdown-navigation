ad46cabd0689b8571f3a720aab3c1e4011922d33
Eric Separovic <ericseparovic07@gmail.com>
2022-11-09 11:24:37 -0300

    Add styles to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,43 @@
 import Logo from "../assets/images/logo.svg";
+import IconMenu from "../assets/images/icon-menu.svg";
+import IconArrowDown from "../assets/images/icon-arrow-down.svg";
 
 function Header() {
   return (
     <header className="header">
-      <div>
+      <div className="header__logo-nav">
         <img src={Logo} alt="logo-snap" />
         <nav className="nav">
-          <a className="nav__item" href="#">
-            Features
-          </a>
-          <a className="nav__item" href="#">
-            Company
-          </a>
-          <a className="nav__item" href="#">
+          <div className="nav__item">
+            <a className="nav__item-a" href="#">
+              Features
+            </a>
+            <img src={IconArrowDown} alt="icon-arrow-down" />
+          </div>
+          <div className="nav__item">
+            <a className="nav__item-a" href="#">
+              Company
+            </a>
+            <img src={IconArrowDown} alt="icon-arrow-down" />
+          </div>
+          <a className="nav__item-a" href="#">
             Careers
           </a>
-          <a className="nav__item" href="#">
+          <a className="nav__item-a" href="#">
             About
           </a>
         </nav>
       </div>
       <div className="header__session">
-        <a href="#">Login</a>
-        <a href="#">Register</a>
+        <a className="header__login" href="#">
+          Login
+        </a>
+        <a className="header__register" href="#">
+          Register
+        </a>
+      </div>
+      <div className="header__icon-menu">
+        <img src={IconMenu} alt="icon-menu" />
       </div>
     </header>
   );
