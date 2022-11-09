261b9d1f0fe3a5b2ac5d8c92b71a2bc80a8273b2
Eric Separovic <ericseparovic07@gmail.com>
2022-11-09 13:00:33 -0300

    Add side menu styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import Logo from "../assets/images/logo.svg";
 import IconMenu from "../assets/images/icon-menu.svg";
 import IconArrowDown from "../assets/images/icon-arrow-down.svg";
+import IconCloseMenu from "../assets/images/icon-close-menu.svg";
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo-nav">
+      <div>
         <img src={Logo} alt="logo-snap" />
+      </div>
+      <div className="header__nav-session">
+        <div className="header__icon-close-menu">
+          <img src={IconCloseMenu} alt="icon-close-menu" />
+        </div>
         <nav className="nav">
           <div className="nav__item">
             <a className="nav__item-a" href="#">
@@ -27,14 +33,15 @@ function Header() {
             About
           </a>
         </nav>
-      </div>
-      <div className="header__session">
-        <a className="header__login" href="#">
-          Login
-        </a>
-        <a className="header__register" href="#">
-          Register
-        </a>
+
+        <div className="header__session">
+          <a className="header__login" href="#">
+            Login
+          </a>
+          <a className="header__register" href="#">
+            Register
+          </a>
+        </div>
       </div>
       <div className="header__icon-menu">
         <img src={IconMenu} alt="icon-menu" />
